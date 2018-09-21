89c6edd58ab008dcdd9c1d8abfdc85608d44a96a
Yomaira Escano <yjescano@gmail.com>
2018-09-21 02:22:54 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,13 +14,13 @@ function postComment() {
   let commenter = document.getElementById("commenterName").value;
   let comment = document.getElementById("commentText").value;
 
-  let commentTemplate = document.getElementById("comment-template").innerHTML;
+  let commentTemplate = document.getElementById("comments-template").innerHTML;
 
-  let templateFn = _.template(commentTemplate);
-
-  let commentsDiv = document.getElementById("comments");
-
-  let templateHTML = templateFn({ 'comment': comment, 'commenter': commenter })
-
-  commentsDiv.innerHTML += templateHTML;
+  // let templateFn = _.template(commentTemplate);
+  // 
+  // let commentsDiv = document.getElementById("comments");
+  // 
+  // let templateHTML = templateFn({ 'comment': comment, 'commenter': commenter })
+  // 
+  // commentsDiv.innerHTML += templateHTML;
 }
